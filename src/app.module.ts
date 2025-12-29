c22e001df80f0d6e5a6c7cce8156e5a223108da4
Opanpan <fanalriansyah@gmail.com>
2025-12-29 22:46:08 +0700

    fix app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
+import * as dotenv from 'dotenv';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './tasks/task.entity';
 import { TasksModule } from './tasks/tasks.module';
 import { ChatModule } from './chat/chat.module';
@@ -9,29 +10,21 @@ import { ConversationRead } from './chat/entities/conversation-read.entity';
 import { Conversation } from './chat/entities/conversation.entity';
 import { Message } from './chat/entities/message.entity';
 import { User } from './chat/entities/user.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+
+dotenv.config();
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get<string>('DB_HOST', 'postgres'),
-        port: config.get<number>('DB_PORT', 5432),
-        username: config.get<string>('POSTGRES_USER', 'postgres'),
-        password: config.get<string>('POSTGRES_PASSWORD', 'postgres'),
-        database: config.get<string>('POSTGRES_DB', 'simpuldb'),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'postgres',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
 
-        entities: [Task, ConversationRead, Conversation, Message, User],
-        synchronize: false,
-
-        migrations: ['dist/database/migrations/*.{js}'],
-        migrationsRun: true,
-      }),
+      entities: [Task, ConversationRead, Conversation, Message, User],
+      synchronize: false,
     }),
     TasksModule,
     ChatModule,
